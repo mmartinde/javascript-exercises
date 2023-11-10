80261a003d46d28203a87a0fa5b145e141233bfb
manuel-martin <mmartinde@alumnos.unex.es>
2023-11-10 09:06:09 +0100

    Completados ejercicios S7 5.filter & 6.find

--- 99_entrega_final/javascript-exercises/S7/4.map/exercise-3.js
+++ 99_entrega_final/javascript-exercises/S7/4.map/exercise-3.js
@@ -1,13 +1,15 @@
-// Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name 
+// Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name
 //y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
 
+const cities = [
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: false, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
 
-const cities = [{isVisited:true, name: 'Tokyo'}, 
-                {isVisited:false, name: 'Madagascar'},
-                {isVisited:true, name: 'Amsterdam'}, 
-                {isVisited:false, name: 'Seul'}];
+const citiesVisited = cities.map((city) =>
+  city.isVisited ? city.name + " Visitado" : city.name
+);
 
-
-const citiesVisited = cities.map (city => city.isVisited ? city.name + (' Visitado'): city.name);
-
-console.log (citiesVisited);
+console.log(citiesVisited);
---
+++ 99_entrega_final/javascript-exercises/S7/6.find/exercise-3.js
@@ -0,0 +1,32 @@
+//Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 'Cucushumushu' y la mutación 'Porompompero'.
+//Una vez que los encuentres, añade la propiedad .mutation al alien con el valor de la mutación encontrada.
+
+const aliens = [
+  { name: "Zalamero", planet: "Eden", age: 4029 },
+  { name: "Paktu", planet: "Andromeda", age: 32 },
+  { name: "Cucushumushu", planet: "Marte", age: 503021 },
+];
+
+const mutations = [
+  {
+    name: "Porompompero",
+    description:
+      "Hace que el alien pueda adquirir la habilidad de tocar el tambor",
+  },
+  {
+    name: "Fly me to the moon",
+    description: "Permite volar, solo y exclusivamente a la luna",
+  },
+  {
+    name: "Andando que es gerundio",
+    description: "Invoca a un señor mayor como Personal Trainer",
+  },
+];
+
+const findAlien = aliens.find(alien => alien.name == 'Cucushumushu');
+const findMutation = mutations.find(mutation =>mutation.name == 'Porompompero');
+
+const newProperty = aliens.map (alien => alien.findAlien? + 'mutation':findMutation.description);
+
+console.log(findAlien,' ', findMutation);
+console.log (newProperty);
