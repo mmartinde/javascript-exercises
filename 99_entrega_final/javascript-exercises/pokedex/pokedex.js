8080efb0769c4194d28955925cd0f55695a233c4
manuel-martin <mmartinde@alumnos.unex.es>
2023-11-20 23:49:32 +0100

    Estilos CSS basicos aplicados a la Pokedex

--- 99_entrega_final/javascript-exercises/pokedex/pokedex.js
+++ 99_entrega_final/javascript-exercises/pokedex/pokedex.js
@@ -1,4 +1,4 @@
-const myMain$$ = document.querySelector("main");
+const myMain$$ = document.querySelector("#pokedex");
 
 const fetchPokemons = async (limit = 150) => {
   try {
@@ -19,38 +19,45 @@ const fetchPokemons = async (limit = 150) => {
   }
 };
 
-
-
-
 const drawPokemons = (mappedPokemons) => {
   myMain$$.innerHTML = "";
   for (const character of mappedPokemons) {
     //console.log(character[1].sprites.other.home.front_default);
-    const divCards$$ = document.createElement("div");
-    
+    const divCards$$ = document.createElement("li");
+
     let pokeId = character[1].id.toString();
     if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
+      pokeId = "00" + pokeId;
     } else if (pokeId.length === 2) {
-        pokeId = "0" + pokeId;
+      pokeId = "0" + pokeId;
     }
     divCards$$.innerHTML = `
-        <div class="pokemon ${character[1].name}">
-            <img src="${character[1].sprites.other.home.front_default}" alt="${character.name}">
-            <p class="id">#${pokeId}</p>
-            <h2 class="name">${character[1].name}</h2>
-            <p class="type01">tipo: ${character[1].types[0].type.name}</p>
-            <p class="ability01">habilidad: ${character[1].abilities[0].ability.name}</p>
-            <p class="base_experience">Level: ${character[1].base_experience}</p>
-            <p class="weight">Weight: ${character[1].weight}</p>
-            <p class="height">Height: ${character[1].height}</p>
+        <div class="pokemon">
+          <p class="pokemon-id-bck">#${pokeId}</p>
+          <div class="pokemon-img">
+          <img src="${character[1].sprites.other.home.front_default}" alt="${character[1].name}">
+          </div>
+          <div class="pokemon-inf">
+            <div class="pokemon-name">
+              <p class="pokemon-id">#${pokeId}</p>
+              <h2 class="name">${character[1].name}</h2>
+            </div>
+            <div class="pokemon-typ">
+              <p class="${character[1].types[0].type.name} type">${character[1].types[0].type.name}</p>
+              <p class="type">${character[1].abilities[0].ability.name}</p>
+            </div>
+            <div class="pokemon-stats">
+              <p class="stat">${character[1].height}M</p>
+              <p class="stat">${character[1].weight}KG</p>
+              <p class="stat">${character[1].base_experience}HP</p>
+            </div>
+          </div>
         </div>
    `;
-   //<p class="ability02">${character[1].abilities[1].ability.name}</p>
-console.log (character[1].order);
-    myMain$$.appendChild(divCards$$); 
-
-}
+    //<p class="ability02">${character[1].abilities[1].ability.name}</p>
+    //console.log(character[1].order);
+    myMain$$.appendChild(divCards$$);
+  }
 
   /*  console.log ("mapa",getPokemonsWithoutMap);
   console.log ("mapeando nombre:",getPokemonsWithoutMap.get(1).name);
@@ -62,15 +69,30 @@ console.log (character[1].order);
   console.log ("mapeando habilidad 2:",getPokemonsWithoutMap.get(1).abilities[1].ability.name);
   console.log ("mapeando weight:",getPokemonsWithoutMap.get(1).weight);
   console.log ("mapeando height:",getPokemonsWithoutMap.get(1).height); */
-  
-    //id: character.get(element).id,
-    //imagen: character.get(element).sprites.front_default,
-    //tipo: character.types[0].type.name,
- 
+
+  //id: character.get(element).id,
+  //imagen: character.get(element).sprites.front_default,
+  //tipo: character.types[0].type.name,
+
   //console.log("funcion map",mappedPokemons);
 };
 
-//catchPokemon();
+const drawInputSearch = (characters) => {
+  const input$$ = document.querySelector("input");
+  input$$.addEventListener("input", () =>
+    searchPokemon(characters, input$$.value)
+  );
+};
+
+const searchPokemon = (characters, filtro) => {
+  console.log("filtro", filtro);
+  const characterArray = Array.from(characters.entries());
+  let filteredCharacter = characterArray.filter(([clave, character]) =>
+    character.name.toLowerCase().includes(filtro.toLowerCase())
+  );
+  drawPokemons(filteredCharacter);
+};
+
 const init = async () => {
   const pokemons = await fetchPokemons(); //get info without map
   //console.log (pokemons);
@@ -78,6 +100,8 @@ const init = async () => {
   const drawPokemon = drawPokemons(pokemons);
 
   //const mappedPokemons = mapPokemons (pokemons);
+
+  drawInputSearch(pokemons);
 };
 
 init();
